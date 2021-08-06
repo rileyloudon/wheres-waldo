6a82367944df4741d82873afd5bcc91319ac7b1a
Riley Loudon <rileyloudon@icloud.com>
2021-08-05 21:43:43 -0500

    setup firebase config

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyAeqwNXZKfTkNi8zXI-Sk-gDMGAuLv5HTA',
+  authDomain: 'wheres-waldo-c710d.firebaseapp.com',
+  projectId: 'wheres-waldo-c710d',
+  storageBucket: 'wheres-waldo-c710d.appspot.com',
+  messagingSenderId: '323021756225',
+  appId: '1:323021756225:web:fe289f8025376aeb0973c0',
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
